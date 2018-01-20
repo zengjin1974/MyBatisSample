3909cdf7d5871795d81cb2375b52bb70354b1b6c
zengjin <zengjin@G08CNXNBFS01881>
2018-01-20 13:02:48 +0800

    sakila

--- src/main/java/com/example/MyBatisSample/TestEmployee.java
+++ src/main/java/com/example/MyBatisSample/TestEmployee.java
@@ -1,3 +1,6 @@
+/*
+ * http://www.casleyconsulting.co.jp/blog-engineer/java/mybatis3mybatisを使ってみよう/
+ */
 package com.example.MyBatisSample;
 
 import java.io.IOException;
@@ -8,13 +11,13 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import com.example.entity.Actor;
-import com.example.entity.ActorMapper;
+import com.example.entity.Employee;
+import com.example.entity.EmployeeMapper;
 
 /**
  * MyBatisを使ってDBにアクセスするサンプルプログラムです.
  */
-public class App {
+public class TestEmployee {
     public static void main(String[] args) {
 
         // resources直下のmybatis-config.xmlを読み込みます(1)
@@ -26,16 +29,15 @@ public class App {
             // SQLセッションを取得します(3)
             try (SqlSession session = factory.openSession()) {
 
-                // ActorテーブルのMapperを取得します(4)
-                ActorMapper map = session.getMapper(ActorMapper.class);
-                // Actorテーブルの主キー（actor_id)が１であるレコードを検索します(5)
-                Actor actor = map.selectByPrimaryKey((short) 1);
+                // EmployeeテーブルのMapperを取得します(4)
+            	EmployeeMapper map = session.getMapper(EmployeeMapper.class);
+                // Employeeテーブルの主キー（Employee_id)が１であるレコードを検索します(5)
+            	Employee emp = map.selectByPrimaryKey((long)1);
 
           // 取得した内容を確認します
-                System.out.println("actor.getActorId    = " + actor.getActorId());
-                System.out.println("actor.getFirstName  = " + actor.getFirstName());
-                System.out.println("actor.getLastName   = " + actor.getLastName());
-                System.out.println("actor.getLastUpdate = " + actor.getLastUpdate());
+                System.out.println("emp.getEmployeeId    = " + emp.getEmployeeId());
+                System.out.println("emp.getEmployeeName  = " + emp.getEmployeeName());
+
             }
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/com/example/MyBatisSample/TestEmployees.java
+++ src/main/java/com/example/MyBatisSample/TestEmployees.java
@@ -9,15 +9,18 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import com.example.entity.Actor;
-import com.example.entity.ActorExample;
-import com.example.entity.ActorMapper;
+import com.example.entity.Employee;
+import com.example.entity.EmployeeExample;
+/*
+ * http://www.casleyconsulting.co.jp/blog-engineer/java/mybatis3mybatisを使ってみよう/
+ */
+import com.example.entity.EmployeeMapper;
 
 /**
  * MyBatisを使ってDBにアクセスするサンプルプログラムです.
  *
  */
-public class App2 {
+public class TestEmployees {
     public static void main(String[] args) {
 
         // resources直下のmybatis-config.xmlを読み込みます
@@ -29,30 +32,29 @@ public class App2 {
             // SQLセッションを取得します
             try (SqlSession session = factory.openSession()) {
 
-                // ActorテーブルのMapperを取得します
-                ActorMapper map = session.getMapper(ActorMapper.class);
+                // EmployeeテーブルのMapperを取得します
+                EmployeeMapper map = session.getMapper(EmployeeMapper.class);
 
-                // Actorテーブルの条件検索用クラスを生成します
-                ActorExample ex = new ActorExample();
+                // Employeeテーブルの条件検索用クラスを生成します
+                EmployeeExample ex = new EmployeeExample();
 
                 // WHERE
-                //    (first_name LIKE 'T%' AND actor_id < 100)
-                //    OR (last_name LIKE 'S%' AND actor_id > 100)
+                //    (employee_name LIKE 'R%')
+                //    OR (employee_name LIKE 'J%')
                 // 検索条件に↑と同等の条件を設定しています
                 // 　  Criteriaを作成し、AND条件を追加する (1)
-                ex.createCriteria().andFirstNameLike("T%").andActorIdLessThan((short)100);
+                ex.createCriteria().andEmployeeNameLike("R%");
                 // 2 OR条件がある場合はExampleのor() メソッドで区切り、OR条件内の条件を追加する
-                ex.or().andLastNameLike("S%").andActorIdGreaterThan((short)100);
+                ex.or().andEmployeeNameLike("J%");
 
                 // 上記の条件でテーブルを検索します
-                List<Actor> actorList = map.selectByExample(ex);
+                List<Employee> empList = map.selectByExample(ex);
 
                 // 取得結果を表示します
-                System.out.println("actor_id, first_name, last_name, last_update");
-                for (Actor actor : actorList) {
+                System.out.println("employee_id, employee_name");
+                for (Employee emp : empList) {
 
-                    System.out.printf("%s, %s, %s, %s \n", actor.getActorId(), actor.getFirstName(), actor.getLastName(),
-                                    actor.getLastUpdate());
+                    System.out.printf("%s, %s \n", emp.getEmployeeId(), emp.getEmployeeName());
                 }
             }
         } catch (IOException e) {
